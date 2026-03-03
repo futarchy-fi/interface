62ba6c358c2c81bc7cd56e34890e93e714b10c49
krandder <azsantos.k@gmail.com>
2026-03-03 18:26:33 +0000

    Fix content hidden behind fixed header on mobile
    PageLayout had lg:mt-20 which only added top margin on large screens.
    The fixed header exists on all screen sizes, so content was hidden
    behind it on smaller viewports. Changed to mt-20 for all breakpoints.

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -11,7 +11,7 @@ const PageLayout = ({ hero, children, contentClassName = '' }) => {
       )}
 
       {/* 2. Main Content Area */}
-      <main className={`bg-white dark:bg-futarchyDarkGray2 w-full flex-grow flex flex-col lg:mt-20`}>
+      <main className={`bg-white dark:bg-futarchyDarkGray2 w-full flex-grow flex flex-col mt-20`}>
         <div className={`container mx-auto px-5 pb-6 ${contentClassName} flex-grow flex flex-col`}>
           {children}
         </div>
